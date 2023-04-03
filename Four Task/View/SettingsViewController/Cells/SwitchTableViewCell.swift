b94c19b8e94b38cbc72a1e854454b414ce4af7f7
Swarogfrinan <Ilyavasilev.wow@gmail.com>
2023-04-03 18:06:33 +0300

    Removed empty parts in SwitchTableViewCell

--- Four Task/View/SettingsViewController/Cells/SwitchTableViewCell.swift
+++ Four Task/View/SettingsViewController/Cells/SwitchTableViewCell.swift
@@ -1,17 +1,11 @@
-//
-//  SwitchTableViewCell.swift
-//  Four Task
-//
-//  Created by Ilya Vasilev on 22.07.2022.
-//
-
 import UIKit
 
 class SwitchTableViewCell: UITableViewCell {
-    //MARK: - Let/var
+    
+    //MARK: - Properties
     static let identifier = "SwitchTableViewCell"
     ///Фоновой цвет позади иконки.
-    private let iconContainer: UIView = {
+    private let iconContainerView: UIView = {
         let view = UIView()
         view.clipsToBounds = true
         view.layer.cornerRadius = 8
@@ -26,49 +20,50 @@ class SwitchTableViewCell: UITableViewCell {
         return imageView
     }()
     ///Текстовое описание настройки
-    private let label: UILabel = {
+    private let descriptionLabel: UILabel = {
         let label = UILabel()
         label.numberOfLines = 1
         return label
     }()
     ///Switch кнопка смена состояния.
-    private let mySwitch: UISwitch = {
+    private let switchControl: UISwitch = {
         let mySwitch = UISwitch()
         mySwitch.onTintColor = .systemRed
         mySwitch.addTarget(SwitchTableViewCell.self, action: #selector(changeAppSwitchValue), for: .valueChanged)
         return mySwitch
     }()
     
-    @objc func changeAppSwitchValue() {
-        if mySwitch.isOn {
-            print("mySwith is ON switched")
-            
-        } else {
-            print("mySwith is OF switched")
-        }
-    }
+  
     //MARK: - Init
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        contentView.addSubview(label)
-        contentView.addSubview(iconContainer)
-        iconContainer.addSubview(iconImageView)
-        contentView.addSubview(mySwitch)
+        contentView.addSubview(descriptionLabel)
+        contentView.addSubview(iconContainerView)
+        iconContainerView.addSubview(iconImageView)
+        contentView.addSubview(switchControl)
         contentView.clipsToBounds = true
         accessoryType = .none
     }
-    
-    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        iconContainerView.backgroundColor = nil
+        descriptionLabel.text = nil
+        iconImageView.image = nil
+        switchControl.isOn = false
+    }
+    
     //MARK: - Layout
+    
     override func layoutSubviews() {
         super.layoutSubviews()
         let size: CGFloat = contentView.frame.size.height - 12
         let imageSize: CGFloat = size/1.5
         ///Icon image in tableView
-        iconContainer.frame = CGRect(
+        iconContainerView.frame = CGRect(
             x: 15,
             y: 6,
             width: size,
@@ -80,34 +75,36 @@ class SwitchTableViewCell: UITableViewCell {
             width: imageSize,
             height: imageSize)
         ///Switch Button Pomadoro
-        mySwitch.sizeToFit()
-        mySwitch.frame = CGRect(
-            x: contentView.frame.size.width - mySwitch.frame.size.width - 20,
-            y: (contentView.frame.size.height - mySwitch.frame.size.height) / 2,
-            width: mySwitch.frame.size.width,
-            height: mySwitch.frame.size.height)
+        switchControl.sizeToFit()
+        switchControl.frame = CGRect(
+            x: contentView.frame.size.width - switchControl.frame.size.width - 20,
+            y: (contentView.frame.size.height - switchControl.frame.size.height) / 2,
+            width: switchControl.frame.size.width,
+            height: switchControl.frame.size.height)
         
-        label.frame = CGRect(
-            x: 25 + iconContainer.frame.size.width,
+        descriptionLabel.frame = CGRect(
+            x: 25 + iconContainerView.frame.size.width,
             y: 0,
-            width: contentView.frame.size.width - 20 - iconContainer.frame.size.width,
+            width: contentView.frame.size.width - 20 - iconContainerView.frame.size.width,
             height: contentView.frame.size.height
         )
     }
-    //MARK: - PrepareForReuse
-    override func prepareForReuse() {
-        super.prepareForReuse()
-        iconContainer.backgroundColor = nil
-        label.text = nil
-        iconImageView.image = nil
-        mySwitch.isOn = false
-    }
-    //MARK: - Configure
+
+    //MARK: - Methods
+    
     public func configure(with model: SettingsSwitchOption) {
-        label.text = model.title
+        descriptionLabel.text = model.title
         iconImageView.image = model.icon
         iconImageView.backgroundColor = model.iconBackgroundColor
-        mySwitch.isOn = model.isOn
+        switchControl.isOn = model.isOn
+    }
+    @objc func changeAppSwitchValue() {
+        if switchControl.isOn {
+            print("mySwith is ON switched")
+            
+        } else {
+            print("mySwith is OF switched")
+        }
     }
 }
 
