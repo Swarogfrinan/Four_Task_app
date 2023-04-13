260792850b2b93d0c6357908b498c409c21dadc0
Swarogfrinan <Ilyavasilev.wow@gmail.com>
2023-04-13 15:12:55 +0300

    Create UIFont+AppFonts.swift

---
+++ Supportings/UIFont+AppFonts.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+extension UIFont {
+    static let thin = UIFont.systemFont(ofSize: 16, weight: .regular)
+    static let standart = UIFont.systemFont(ofSize: 18)
+    static let big = UIFont.systemFont(ofSize: 23, weight: .regular)
+    static let huge = UIFont.systemFont(ofSize: 24, weight: .semibold)
+    static let enormous = UIFont.systemFont(ofSize: 30, weight: .bold)
+}
