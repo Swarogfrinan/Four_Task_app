3a7bda9b1beb3ecdd87503386c8809e116e1494d
Swarogfrinan <Ilyavasilev.wow@gmail.com>
2023-04-13 11:54:06 +0300

    Create Constants.swift

---
+++ Supportings/Constants.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+struct Constants {
+    struct NavigationBarTitle {
+        static let fourTask = "Four Task"
+        static let Tomato = "Tomato"
+    }
+    
+    struct ErrorType {
+        static let basic = "Something went wrong"
+        static let noInternet = "No Internet"
+    }
+    
+    struct ButtonTitle {
+        static let refresh = "Refresh"
+    }
+    
+}
