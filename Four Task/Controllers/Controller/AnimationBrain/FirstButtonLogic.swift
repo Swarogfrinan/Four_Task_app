d40dea8012488579ed8af93827cf9fb9bc613650
Swarogfrinan <Ilyavasilev.wow@gmail.com>
2023-04-14 22:16:21 +0300

    Renamed Methods in FirstButton

--- Four Task/Controllers/Controller/AnimationBrain/FirstButtonLogic.swift
+++ Four Task/Controllers/Controller/AnimationBrain/FirstButtonLogic.swift
@@ -6,92 +6,91 @@ var stopDateFirst: Date?
 var startDateFirst: Date?
 var scheduledTimerFirst: Timer!
 
-//MARK: - Extension
+//MARK: - FirstTask
 extension FourTaskViewController {
     ///Подтягивание 1 таймера из бэкграунда если он был нажат последним.
-    func firstRefreshBackgroundTimer() {
+    func refreshFirstBackgroundTimer() {
         if timerStarted.firstTimerStarted {
-            startTimerOne()
+            startFirstTimer()
         } else {
-            stopTimerOne()
+            pauseFirstTimer()
             if let start = startDateFirst {
                 if let stop = stopDateFirst {
-                    let time = calcRestartTimeOne(start: start, stop: stop)
+                    let time = calculatedFirstRestartDate(start: start, stop: stop)
                     let diff = Date().timeIntervalSince(time)
-                    setTimeLabelOne(Int(diff))
+                    setupFirstDefaultLabel(Int(diff))
                 }
             }
         }
     }
     //MARK: - Start
-    func firstTask() {
+    func fullStartFirstTimer() {
         print("Нажатие 1 кнопки")
         if timerCounting == true {
             print("TimerCounting = true в 1 кнопке")
-            setStopTimeOne(date: Date())
+            setFirstKeyPause(date: Date())
             canFirstButtonAnimatedStopped()
-            stopTimerOne()
+            pauseFirstTimer()
             print("1 таймер остановлен. UI отработал. ")
         } else {
             if let stop = stopDateFirst {
-                let restartTime = calcRestartTimeOne(start: startDateFirst!, stop: stop)
-                setStopTimeOne(date: nil)
-                setStartTimeOne(date: restartTime)
+                let restartTime = calculatedFirstRestartDate(start: startDateFirst!, stop: stop)
+                setFirstKeyPause(date: nil)
+                setFirstKeyStart(date: restartTime)
             } else {
-                setStartTimeOne(date: Date())
+                setFirstKeyStart(date: Date())
             }
             canFirstButtonAnimatedStarted()
-            startTimerOne()
+            startFirstTimer()
         }
     }
     ///Запуск Stop Action Timer с проверкой есть ли пауза.
-    fileprivate func startTimerOne() {
-        scheduledTimerFirst = Timer.scheduledTimer(timeInterval: 0.1 , target: self, selector:    #selector(refreshValueOne), userInfo: nil, repeats: true)
-        setTimerCountingOne(true)
+    fileprivate func startFirstTimer() {
+        scheduledTimerFirst = Timer.scheduledTimer(timeInterval: 0.1 , target: self, selector:    #selector(refreshFirstDefaultValue), userInfo: nil, repeats: true)
+        setFirstKeyCounting(true)
         canFirstButtonAnimatedStarted()
     }
     //MARK: - STOP
-    fileprivate func stopActionOne() {
-        
+    fileprivate func  pauseFirstAction() {
         if stopDateFirst != nil {
-            let restartTime = calcRestartTimeOne(start: startDateFirst!, stop: stopDateFirst!)
-            setStopTimeOne(date: nil)
-            setStartTimeOne(date: restartTime)
+            let restartTime = calculatedFirstRestartDate(start: startDateFirst!, stop: stopDateFirst!)
+            setFirstKeyPause(date: nil)
+            setFirstKeyStart(date: restartTime)
         } else {
-            setStartTimeOne(date: Date())
+            setFirstKeyStart(date: Date())
         }
-        startTimerOne()
+        startFirstTimer()
     }
     
-    fileprivate func stopTimerOne() {
+    fileprivate func pauseFirstTimer() {
         if scheduledTimerFirst != nil {
             scheduledTimerFirst.invalidate()
-            setTimerCountingOne(false)
+            setFirstKeyCounting(false)
         }
     }
     
-    fileprivate func calcRestartTimeOne(start: Date, stop: Date) -> Date {
+    fileprivate func calculatedFirstRestartDate(start: Date, stop: Date) -> Date {
         let diff = start.timeIntervalSince(stop)
         return Date().addingTimeInterval(diff)
     }
-    //MARK: - Set userdefaults Keys.
-    fileprivate func setStopTimeOne(date: Date?) {
+    //MARK: - Setyp UserDefaults Keys.
+    fileprivate func setFirstKeyPause(date: Date?) {
         stopDateFirst = date
         userDefaults.set(stopDateFirst, forKey: FirstKeys.stop.rawValue)
         print("Установлен setStopTimeOne по Stop_keys0 в FirstButtonLogic.")
     }
-    fileprivate func setStartTimeOne(date: Date?) {
+    fileprivate func setFirstKeyStart(date: Date?) {
         startDateFirst = date
         userDefaults.set(startDateFirst, forKey: FirstKeys.start.rawValue)
         print("Установлен setStartTimeOne по Start_keys0 в FirstButtonLogic.")
     }
-    fileprivate func setTimerCountingOne(_ value: Bool) {
+    fileprivate func setFirstKeyCounting(_ value: Bool) {
         timerStarted.firstTimerStarted = value
         userDefaults.set(timerStarted.firstTimerStarted, forKey: FirstKeys.counting.rawValue)
         print("Установлен setTimerCountingOne по Counting_keys0 в FirstButtonLogic.")
     }
     ///Установить лейблы и цвета по отформатированному времени.
-    fileprivate func setTimeLabelOne(_ value: Int) {
+    fileprivate func setupFirstDefaultLabel(_ value: Int) {
         let time = secToHourMinutesSeconds(seconds: value)
         let timeString = makeTimeString(hours: time.0, minutes: time.1, seconds: time.2)
         clockLabel.text = timeString
@@ -99,25 +98,25 @@ extension FourTaskViewController {
         taskNumberFirstLabel.textColor = .systemYellow
     }
   
-    //MARK: - ResetAction
+    //MARK: - Reset Default Action
     ///Сбросить Date  и выставить лейблы на 0
-    func resetActionOne() {
-        setStopTimeOne(date: nil)
-        setStartTimeOne(date: nil)
+    func resetFirstActionToDefault() {
+        setFirstKeyPause(date: nil)
+        setFirstKeyStart(date: nil)
         clockLabel.text = makeTimeString(hours: 0, minutes: 0, seconds: 0)
         taskNumberFirstLabel.text = makeTimeString(hours: 0, minutes: 0, seconds: 0)
-        stopTimerOne()
+        pauseFirstTimer()
         //    animateFirstViewStop()
     }
     //MARK: - RefreshValue
     ///Обновление данных, если что-то не так. Установка лейблу нуля.
-    @objc private func refreshValueOne() {
+    @objc private func refreshFirstDefaultValue() {
         if let start = startDateFirst {
             let diff = Date().timeIntervalSince(start)
-            setTimeLabelOne(Int(diff))
+            setupFirstDefaultLabel(Int(diff))
         } else {
-            stopTimerOne()
-            setTimeLabelOne(0)
+            pauseFirstTimer()
+            setupFirstDefaultLabel(0)
         }
     }
 }
