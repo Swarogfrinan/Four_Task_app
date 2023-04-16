25356bc13b48e61e53f74b185e66e87ba04ade8b
Swarogfrinan <Ilyavasilev.wow@gmail.com>
2023-04-16 12:46:41 +0300

    Create FirstButton.swift

---
+++ Four Task/Controllers/Controller/AnimationBrain/FirstButton.swift
@@ -0,0 +1,68 @@
+import UIKit
+
+class FirstButton: ButtonLogicProtocol {
+    var stopDate: Date?
+    
+    var startDate: Date?
+    
+    var scheduletTimer: Timer!
+    
+    func refreshBackgroundTimer() {
+    
+    }
+    
+    func fullLauchTimer() {
+        <#code#>
+    }
+    
+    func startTimer() {
+        <#code#>
+    }
+    
+    func pauseAction() {
+        <#code#>
+    }
+    
+    func pauseTimer() {
+        <#code#>
+    }
+    
+    func canButtonHaveAnimatedStart() {
+        <#code#>
+    }
+    
+    func canButtonHaveAnimatedStop() {
+        <#code#>
+    }
+    
+    func buttonStartedAnimation() {
+        <#code#>
+    }
+    
+    func buttonPauseAnimation() {
+        <#code#>
+    }
+    
+    func setupDefaultLabel() {
+        <#code#>
+    }
+    
+    func setKeyPause() {
+        <#code#>
+    }
+    
+    func setKeyStart() {
+        <#code#>
+    }
+    
+    func setKeyCounting() {
+        <#code#>
+    }
+    
+    func resetActionToDefault() {
+        <#code#>
+    }
+    
+    
+}
+
