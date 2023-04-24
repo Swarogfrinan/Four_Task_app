a83787899cb2bb7d8db69a3f118a135f1bf5104e
Swarogfrinan <Ilyavasilev.wow@gmail.com>
2023-04-24 14:12:37 +0300

    Create FirstAnimation.swift

---
+++ Four Task/Model/FourTask/Animations/FirstAnimation.swift
@@ -0,0 +1,11 @@
+//import Foundation
+//import UIKit
+//
+//
+//
+////MARK: - First Case
+//class viewModelFirst: AnimationProtocol, ButtonDelegateProtocol {
+//    let vc = FourTaskViewController()
+//    
+//   
+//}
